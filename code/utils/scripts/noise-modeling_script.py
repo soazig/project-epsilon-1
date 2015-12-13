4a7c1001ca7b089164739eca255e272db43a6335
soazig <soazig.kaam@gmail.com>
2015-12-13 14:02:03 -0800

    added script for niose modeling

--- code/utils/scripts/noise-modeling_script.py
+++ code/utils/scripts/noise-modeling_script.py
@@ -18,12 +18,15 @@ import pprint as pp
 
 #Specicy the path for functions
 sys.path.append(os.path.join(os.path.dirname(__file__), "../functions/"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "./"))
 from diagnostics import *
 from glm import *
+from plot_mosaic import * 
+#from mask_functions import *
 
 # Locate the paths
 project_path = '../../../'
-data_path = '../../data/ds005/' 
+data_path = project_path+'data/ds005/' 
 path_dict = {'data_filtered':{ 
 			      'type' : 'filtered',
 			      'bold_img_name' : 'filtered_func_data_mni.nii.gz',
@@ -43,27 +46,27 @@ subject_list = ['1']
 
 d = path_dict['data_original'] #OR path_dict['data_filtered']
 
-images_paths = [('ds005_' + d['type'] +'_sub' + s.zfill(3) + '_t1r' + r, \
+images_paths = [('ds005' + d['type'] +'_sub' + s.zfill(3) + '_t1r' + r, \
                  data_path + 'sub%s/'%(s.zfill(3)) + d['run_path'] \
                  + 'task001_run%s/%s' %(r.zfill(3), d['bold_img_name'])) \
                  for r in run_list \
                  for s in subject_list]
 
 # Create the needed directories if they do not exist
-#dirs = ['../../fig/','../../fig/linear_projection',\
-#        '../../txt_output','../../txt_output/linear_model']
-#for d in dirs:
-#    if not os.path.exists(d):
-#        os.makedirs(d)
+dirs = [project_path+'fig/',project_path+'fig/linear_projection',\
+        project_path+'fig/pca',\
+        project_path+'txt_output/',project_path+'txt_output/design_matrix/',\
+	project_path+'txt_output/linear_model/']
+for d in dirs:
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 # set gray colormap and nearest neighbor interpolation by default
 plt.rcParams['image.cmap'] = 'gray'
 plt.rcParams['image.interpolation'] = 'nearest'
-nice_cmap_values = np.loadtxt('scripts/actc.txt')
-nice_cmap = colors.ListedColormap(nice_cmap_values, 'actc')
 
 # Create the mask				                
-thres = 375 #From analysis of the histograms   
+thres = 300 #From analysis of the histograms   
 
 for image_path in images_paths:
     name = image_path[0]
@@ -71,8 +74,16 @@ for image_path in images_paths:
     data = img.get_data()
     vol_shape = data.shape[:-1]
     mean_data = np.mean(data, axis=-1)
+    in_brain_mask = mean_data > thres
     # Select the voxels in the brain
-    in_brain_mask = mean_data > thres 
+    plt.imshow(plot_mosaic(mean_data), cmap='gray', alpha=1)
+    plt.colorbar()
+    plt.contour(plot_mosaic(in_brain_mask),colors='blue')
+    plt.title('In brain voxel mean values' + '\n' +  (str(name)))
+    plt.savefig(project_path+'/fig/BOLD/%s_mean_voxels_countour.png'\
+                %(str(name)))
+    plt.show()
+    pdb.set_trace()
     n_trs = data.shape[-1]
     # Smoothing with Gaussian filter
     # Smooth by 2 voxel SD in all three spatial dimensions
@@ -80,22 +91,21 @@ for image_path in images_paths:
     # Convolution with 1 to 4 conditions
     convolved = []
     for i in range(1,5):
-        convolved.append(np.loadtxt(\
- 	      'txt_files/ds005_sub001_t1r1_conv' + str(i) + '_high_res.txt'))
-#        convolved = np.loadtxt('txt_files/ds005_sub001_t1r1_conv0.txt')
-     
+        convolved = np.loadtxt(\
+	project_path+\
+	'txt_output/conv_normal/%s_conv001_canonical.txt'%(str(name)))
     #Create design matrix X
     P = 7 #number of regressors of X 
     X = np.ones((n_trs, P))
-    for i in range(4):
+    for i in range(1,5):
         X[:,i] = convolved[i]
     linear_drift = np.linspace(-1, 1, n_trs)
-    X[:,4] = linear_drift
+    X[:,5] = linear_drift
     quadratic_drift = linear_drift ** 2
     quadratic_drift -= np.mean(quadratic_drift)
-    X[:,5] = quadratic_drift
+    X[:,6] = quadratic_drift
     # Save the design matrix
-    np.savetxt('../../txt_output/linear_model/%s_design_matrix.txt'%(str(name)), X)
+    np.savetxt(project_path+'txt_output/design_matrix/%s_design_matrix.txt'%(str(name)), X)
 #    X = np.loadtxt('ds005_sub001_t1r1_design_matrix.txt')
 #    plt.imshow(X, aspect=0.05)
 #    plt.colorbar()
@@ -105,51 +115,29 @@ for image_path in images_paths:
 #    plt.close()
 
     # Model
-    reg_str = ['Task', 'Gain', 'Loss', 'Distance', 'Linear Drift',\
+    reg_str = ['Intercept','Task', 'Gain', 'Loss', 'Distance', 'Linear Drift',\
                 'Quadratic drift']
     in_brain_tcs = smooth_data[in_brain_mask, :]
     Y = in_brain_tcs.T
     betas = npl.pinv(X).dot(Y)
-    np.savetxt('../../txt_output/linear_model/%s_betas.txt'%(str(name)), betas)
+    #np.savetxt('../../txt_output/linear_model/%s_betas.txt'%(str(name)), betas)
 ##    betas = np.loadtxt('%s_betas.txt'%(str(name)))
-
     betas_vols = np.zeros(vol_shape + (P,))
     betas_vols[in_brain_mask] = betas.T
 #    # set regions outside mask as missing with np.nan
     mean_data[~in_brain_mask] = np.nan
     betas_vols[~in_brain_mask] = np.nan
-    for k in range(5):
-        grid_1D = np.zeros((6*6, vol_shape[0], vol_shape[1]))
-        grid_1D_beta = grid_1D
-        grid_2D = np.zeros((6*vol_shape[0], 6*vol_shape[1]))
-        grid_2D_beta  = grid_2D
-        for z in range(vol_shape[2]):
-            grid_1D[z, :, :] = mean_data[:, ::-1, z].T
-            grid_1D_beta[z, :, :] = betas_vols[:, :, z, k].T
-            #plt.title('Slice #%s (z axis) - mean values of voxels \n' %(z+1)\
-            #          + str(name))
-            d = '../plots/fig/linear_model/model/%s/'%str(name)
-            if not os.path.exists(d): os.makedirs(d)
-            #plt.savefig(d + str(name) + '_mean_vol_mask_slice' \
-       	#            + str(z+1) + '.png')
-            z = 0
-       	    for i in range(6):
-	        for j in range(6):
-		    if z < vol_shape[2]:
-		        grid_2D[i*vol_shape[0]:(i+1)*vol_shape[0],\
-		        j*vol_shape[1]:(j+1)*vol_shape[1]] = grid_1D[z]
-		        grid_2D_beta[i*vol_shape[0]:(i+1)*vol_shape[0],\
-		        j*vol_shape[1]:(j+1)*vol_shape[1]] = grid_1D_beta[z]
-                        z += 1
-	plt.imshow(grid_2D, cmap='gray', alpha=0.5)
-       	plt.imshow(grid_2D_beta, cmap=nice_cmap, alpha=0.5)
+    nice_cmap_values = np.loadtxt('actc.txt')
+    nice_cmap = colors.ListedColormap(nice_cmap_values, 'actc')
+    for k in range(1,P+1):
+        betas_vols[...,k].shape
+	plt.imshow(plot_mosaic(mean_data), cmap='gray', alpha=1)
+       	plt.imshow(plot_mosaic(betas_vols[...,k]), cmap=nice_cmap, alpha=1)
 	plt.colorbar()
-        plt.title('In brain voxel activation related to ' \
-	    + str(cond_str[k]) + '\n' + (str(name)))
-	plt.savefig('../plots/fig/linear_model/model/%s_noise_modelX%s'\
-	            %(str(name), str(k))+'.png')
-        #plt.show()
-                         
+        plt.title('Beta values for brain voxel related to ' \
+	    + str(reg_str[k]) + '\n' + (str(name)))
+        plt.show()
+	pdb.set_trace()                 
 #    for i in range(P):
 #        plt.imshow(betas_vols[:, :, 18, i], cmap='gray', alpha=0.5)
 #        plt.colorbar()
@@ -175,20 +163,20 @@ for image_path in images_paths:
         plt.imshow(projection_vols[:, :, 18, i])   
         plt.colorbar()
         plt.title('PCA - Projection on U' + str(i) + ' vector from SVD \n ' + str(name))
-        plt.savefig('../plots/fig/linear_model/pca/%s_projection_U%s' %(str(name), str(i))\
+        plt.savefig(project_path+'fig/linear_model/pca/%s_projection_U%s' %(str(name), str(i))\
 	+ '.png')
         plt.close()
     #TODO: remove the mean
     s = []
     for i in S:
         s.append(i/np.sum(S))
-    np.savetxt('../plots/fig/linear_model/pca/%s_variance_explained' %str(name) + '.txt', np.array(s[:50]))
+    np.savetxt(project_path+'fig/linear_model/pca/%s_variance_explained' %str(name) + '.txt', np.array(s[:50]))
     ind = np.arange(len(s[1:50]))
     plt.bar(ind, s[1:50], width=0.25)
     plt.xlabel('S indices')
     plt.ylabel('Explained variance in percent')
     plt.title('Variance explained graph \n' + str(name))
-    plt.savefig('../plots/fig/linear_model/pca/%s_variance_explained' %str(name) + '.png')
+    plt.savefig(project_path+'fig/linear_model/pca/%s_variance_explained' %str(name) + '.png')
     #plt.show()
     
     X_pca = np.ones((n_trs, P+1))
@@ -212,7 +200,7 @@ for image_path in images_paths:
         plt.imshow(b_pca_vols[:, :, 18, i])
         plt.colorbar()
 	plt.title('In brain voxel model - projection on X%s - PCA \n %s'%(i,str(name)))
-	plt.savefig('../plots/fig/linear_model/pca/%s_noise_modelX%s_PCA'%(str(name), str(i))+\
+	plt.savefig(project_path+'fig/linear_model/pca/%s_noise_modelX%s_PCA'%(str(name), str(i))+\
 	'.png')
 	#plt.show()
         plt.close()
@@ -222,7 +210,7 @@ for image_path in images_paths:
     residuals_pca = Y - X_pca.dot(B_pca)
     df_pca = X_pca.shape[0] - npl.matrix_rank(X_pca)
     MRSS_pca = np.sum(residuals_pca ** 2 , axis=0) / df_pca
-    np.savetxt('../plots/fig/linear_model/pca/%s_mean_mrss_vals.txt'%(str(name)),\
+    np.savetxt(project_path+'fig/linear_model/pca/%s_mean_mrss_vals.txt'%(str(name)),\
                (np.mean(MRSS), np.mean(MRSS_pca)))
     pdb.set_trace()
 
