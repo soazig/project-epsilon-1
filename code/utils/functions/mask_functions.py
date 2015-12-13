7653d177719ae33dad5cb54f05fa72d1ed95ce8f
soazig <soazig.kaam@berkeley.edu>
2015-12-12 20:40:53 -0800

    added function for the template

--- code/utils/functions/mask_functions.py
+++ code/utils/functions/mask_functions.py
@@ -4,18 +4,24 @@ A collection of functions to make masks on data.
 See test_* functions in this directory for nose tests
 """
 from __future__ import print_function, division
+
 import sys, os, pdb
 import numpy as np
 import nibabel as nib
-from numpy.testing import assert_array_equal
+import numpy.linalg as npl
 
-def make_mask_filtered_data(img_path, mask_path):
+from os.path import splitext
+from numpy.testing import assert_array_equal
+from scipy.ndimage import affine_transform
+
+
+def make_mask_filtered_data(func_path, mask_path):
     """Return the masked filtered data
 
     Parameters
     ----------
-    img_path: string
-    	path to the 4D data
+    func_path: string
+    	  path to the 4D data
     
     mask_path: string
         path to the mask function
@@ -26,7 +32,7 @@ def make_mask_filtered_data(img_path, mask_path):
         masked filtered data
     
     """
-    func_img = nib.load(img_path)
+    func_img = nib.load(func_path)
     mask_img = nib.load(mask_path)
     mask = mask_img.get_data()
     func_data = func_img.get_data()
@@ -37,6 +43,31 @@ def make_mask_filtered_data(img_path, mask_path):
     # nib.save(masked_func, 'masked_' + img_name )
     return masked_func
 
+def resample_filtered_data(func_path, template_path):
+    """Resample template to filtered functional dataset
+    
+    Parameters
+    ----------
+    func_path: string
+        path of the 4D filtered data to resample
+    template_path: string
+        path to the template to apply on the data
+    
+    """
+    filtered_func = nib.load(func_path) 
+    filtered_shape = filtered_func.shape[:3]
+    template_img = nib.load(mni_fname)
+    template_data = template_img.get_data()
+    vox2vox = npl.inv(template_img.affine).dot(filtered_func.affine)
+    M, trans = nib.affines.to_matvec(vox2vox)
+    resampled = affine_transform(template_data, M, trans,
+                                 output_shape=filtered_shape)
+    froot, ext = splitext(mni_fname)
+    new_name = froot + '_2mm' + ext
+    new_img = nib.Nifti1Image(resampled, filtered_func.affine,
+                              template_img.header)
+    #nib.save(new_img, new_name) 
+    return new_img
 
 def apply_mask(data_3d, mask_3d):
     """Apply mask on a 3D image and return the masked data
@@ -48,7 +79,7 @@ def apply_mask(data_3d, mask_3d):
     mask_3d: 3D numpy array   
         The mask for the corresponding data_3d
         has 1 for the positions to select and 
-	0 for the positions to filter out.
+      	0 for the positions to filter out.
     Return
     ------
     masked_func: 3D numpy array
