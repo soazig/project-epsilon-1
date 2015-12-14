d51e52c4705880e2344b5a995388f5ea08399098
soazig <soazig.kaam@gmail.com>
2015-12-13 22:12:58 -0800

    improved mask_filtered_data function

--- code/utils/scripts/mask_filtered_data.py
+++ code/utils/scripts/mask_filtered_data.py
@@ -39,8 +39,11 @@ def make_mask_filtered_data(func_path, mask_path):
     # Make data 4D to prepare for "broadcasting"
     mask = np.reshape(mask, mask.shape + (1,))
     # "Broadcasting" expands the final length 1 dimension to match the func data
+    masked_name = 'masked_' + str(func_path.split('/')[-1])
+    masked_func_path = os.path.join(\
+       str('/'.join(func_path.split('/')[:-1])), masked_name)
     masked_func = nib.Nifti1Image(func_data, func_img.affine, func_img.header)
-    # nib.save(masked_func, 'masked_' + img_name )
+    nib.save(masked_func, masked_func_path)
     return masked_func
 
 if __name__=='__main__':
@@ -49,9 +52,7 @@ if __name__=='__main__':
     project_path+\
     'data/ds005/sub001/model/model001/task001_run001.feat/filtered_func_data_mni.nii.gz'
     img = nib.load(func_path) 
-    mask_path = project_path+'data/mni_icbm152_t1_tal_nlin_asym_09c_mask_2mm.nii'
+    mask_path = project_path+\
+       'data/mni_icbm152_t1_tal_nlin_asym_09c_mask_2mm.nii'
     masked_data = make_mask_filtered_data(func_path, mask_path)
-    new_data_path = project_path+\
-        'data/ds005/sub001/model/model001/task001_run001.feat/masked_filtered_func_data_mni.nii.gz'
-    nib.save(masked_data, new_data_path)
-    #pdb.set_trace()
+    pdb.set_trace()
